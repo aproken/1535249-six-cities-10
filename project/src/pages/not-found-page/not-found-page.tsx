738e958dd0001344359d33a7640faaa5e387e9e2
aproken <aproken@yandex.ru>
2022-07-17 23:43:00 +0300

    feat: Маршрут по тайным тропам

---
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { LogoLocation, Logo } from '../../components/logo/logo';
+
+export default function NotFoundPage(): JSX.Element {
+  return (
+    <div className="page page--gray page--main">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <Logo location={LogoLocation.header} width={81} height={41} />
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--index page__main--index-empty">
+        <div className="cities">
+          <div className="cities__places-container cities__places-container--empty container">
+            <section className="cities__no-places">
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">404. Page not found</b>
+                <Link to="/">Вернуться на главную</Link>
+              </div>
+            </section>
+            <div className="cities__right-section"></div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
--- project/src/pages/property-page/property.tsx
+++ project/src/pages/property-page/property.tsx
@@ -1,3 +1,5 @@
+import { LogoLocation, Logo } from '../../components/logo/logo';
+
 export default function PropertyPage(): JSX.Element {
   return (
     <div className="page">
@@ -5,9 +7,7 @@ export default function PropertyPage(): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              <Logo location={LogoLocation.header} width={81} height={41} />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
